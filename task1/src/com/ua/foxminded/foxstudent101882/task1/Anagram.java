5be3e174e4f5e941222e65806dc3c12579cbfd43
Shidenkain1 <Stanislavdnepr1337@gmail.com>
2022-07-15 10:43:40 +0300

    Changed name of two methods in class Anagram Extracted String input into Main class Changed input String to class Scanner Changed reverse method without additional variables (i, j etc)

--- task1/src/com/ua/foxminded/foxstudent101882/task1/Anagram.java
+++ task1/src/com/ua/foxminded/foxstudent101882/task1/Anagram.java
@@ -1,63 +1,39 @@
 package com.ua.foxminded.foxstudent101882.task1;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Anagram {
 
-    public static String makesAnagram(String text) {
+    public static String reverseString(String text) {
 
-        char[] textCharArray = text.toCharArray();
-        StringBuilder reverseText = new StringBuilder();
 
-        reverseText.reverse();
-
-        char[] reverseTextCharArray = reverseText.toString().toCharArray();
-        char temp;
-        for (int i = 0; i < textCharArray.length; ) {
-            for (int j = textCharArray.length - 1; j > i; ) {
-
-                if (Character.isAlphabetic(textCharArray[i])) {
-                    if (Character.isAlphabetic(textCharArray[j])) {
-
-                        temp = textCharArray[i];
-                        textCharArray[i] = textCharArray[j];
-                        textCharArray[j] = temp;
-                        i++;
-                        j--;
-
-                    } else {
-                        j--;
-                    }
-                } else {
-                    i++;
-                }
+        StringBuffer result = new StringBuffer();
+        Matcher m = Pattern.compile("\\p{L}+").matcher(text);
+            while (m.find()) {
+                String replacement = new StringBuilder(m.group()).reverse().toString();
+                m.appendReplacement(result, replacement);
             }
-            break;
+
+        m.appendTail(result);
+        return result.toString();
+
         }
-        return String.valueOf(textCharArray);
-    }
 
-    public static String splittingStringAnagram() {
+    public static String splitStringByWords(String text) {
 
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-
-        try {
-            String[] wordsArray = read.readLine().split(" ");
+            String[] wordsArray = text.split(" ");
 
             StringBuilder result = new StringBuilder();
             for (int i = 0; i < wordsArray.length; i++) {
 
-                result.append(makesAnagram(wordsArray[i]));
+                result.append(reverseString(wordsArray[i]));
                 result.append(" ");
             }
 
             return result.toString();
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
         }
 
     }
-}
+
--- task1/src/com/ua/foxminded/foxstudent101882/task1/Main.java
+++ task1/src/com/ua/foxminded/foxstudent101882/task1/Main.java
@@ -1,11 +1,15 @@
 package com.ua.foxminded.foxstudent101882.task1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
         Anagram anagram = new Anagram();
-        System.out.println(anagram.splittingStringAnagram());
+        System.out.println(anagram.splitStringByWords(scan.nextLine()));
 
 
     }
